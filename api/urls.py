ce5648faa2179a77375dccde4ca5221510aceca8
Oscar <orojas12@outlook.com>
2021-12-13 22:48:48 -0700

    add urls for Pet resources

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from api import views
 
 urlpatterns = [
     path("users/", views.UserList.as_view()),
-    path("users/<str:pk>", views.UserDetail.as_view()),
+    path("users/<str:pk>/", views.UserDetail.as_view()),
     path("pets/", views.PetList.as_view()),
+    path("pets/<int:pk>/", views.PetDetail.as_view()),
+    path("pets/<int:pk>/photos/", views.PetPhotoList.as_view()),
 ]
