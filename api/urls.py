924d606bdc2a207b23c884811b8913c90df30b9b
Oscar <orojas12@outlook.com>
2021-12-18 12:34:55 -0700

    rename url patterns

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path("users/", views.UserList.as_view(), name="users"),
-    path("users/<str:pk>/", views.UserDetail.as_view(), name="user"),
-    path("pets/", views.PetList.as_view()),
-    path("pets/<int:pk>/", views.PetDetail.as_view()),
-    path("pets/<int:pk>/photos/", views.PetPhotoList.as_view()),
+    path("users/", views.UserList.as_view(), name="user-list"),
+    path("users/<str:pk>/", views.UserDetail.as_view(), name="user-detail"),
+    path("pets/", views.PetList.as_view(), name="pet-list"),
+    path("pets/<int:pk>/", views.PetDetail.as_view(), name="pet-detail"),
+    path(
+        "pets/<int:pk>/photos/", views.PetPhotoList.as_view(), name="pet-detail-photos"
+    ),
 ]
