e2a2443d8c3606cd9452c155685455fc91be3a6a
Oscar <orojas12@outlook.com>
2021-12-18 00:31:30 -0700

    add names to url patterns for UserList and UserDetail views

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path("users/", views.UserList.as_view()),
-    path("users/<str:pk>/", views.UserDetail.as_view()),
+    path("users/", views.UserList.as_view(), name="users"),
+    path("users/<str:pk>/", views.UserDetail.as_view(), name="user"),
     path("pets/", views.PetList.as_view()),
     path("pets/<int:pk>/", views.PetDetail.as_view()),
     path("pets/<int:pk>/photos/", views.PetPhotoList.as_view()),
