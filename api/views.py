d0af97f3ea1a2ba5b730a8c805fce370904e37e9
Oscar <orojas12@outlook.com>
2021-12-31 13:58:36 -0700

    delete unnecessary PetDetailPhotoList view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 import boto3
 from django.http.response import Http404
 from django.contrib.auth.models import User
@@ -7,10 +8,7 @@ from rest_framework.response import Response
 from api.models import Pet, Photo
 from api.serializers import PhotoSerializer, UserSerializer, PetSerializer
 
-s3 = boto3.resource("s3")
-S3_BUCKET = "lostneighborhoodpets.com"
 
-# Create your views here.
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -29,18 +27,3 @@ class PetList(generics.ListCreateAPIView):
 class PetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
-
-
-class PetDetailPhotoList(views.APIView):
-    def get(self, request, pk):
-        photos = Photo.objects.filter(pet=pk)
-        serializer = PhotoSerializer(photos, many=True)
-        return Response(data=serializer.data)
-
-    def post(self, request, pk):
-        serializer = PhotoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
