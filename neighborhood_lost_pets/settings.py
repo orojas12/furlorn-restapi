4012e4c69cfc37799572d2acac18152a428694a1
Oscar <orojas12@outlook.com>
2022-02-20 16:00:49 -0700

    include missing import

--- neighborhood_lost_pets/settings.py
+++ neighborhood_lost_pets/settings.py
@@ -14,6 +14,8 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
