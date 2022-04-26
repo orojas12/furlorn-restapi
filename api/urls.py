cc6204a4d8a657c3386e8f7cc04eacc49699a871
Oscar <orojas12@outlook.com>
2022-04-26 16:09:54 -0600

    edit routes to reflect changes in views

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from api import views
 
 urlpatterns = [
-    path("users/", views.UserList.as_view(), name="user-list"),
-    path("users/<str:pk>/", views.UserDetail.as_view(), name="user-detail"),
-    path("pets/", views.PetList.as_view(), name="pet-list"),
-    path("pets/<int:pk>/", views.PetDetail.as_view(), name="pet-detail"),
+    path("users/<str:username>", views.UserView.as_view(), name="user"),
+    path("posts", views.PostsView.as_view(), name="posts"),
+    path("posts/<str:pk>", views.PostView.as_view(), name="post"),
+    path("register", views.RegisterUserView.as_view(), name="register_user"),
+    path("login", views.LoginUserView.as_view(), name="login_user"),
+    path("logout", views.LogoutUserView.as_view(), name="logout_user"),
 ]
