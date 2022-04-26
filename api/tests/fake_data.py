850de0519ed1655dc314aafd823737c03a21aff0
Oscar <orojas12@outlook.com>
2022-04-26 16:02:45 -0600

    add new FakeData classes
    These are major changes to the fake data module, including the addition
    of FakeData classes which offer a more concise, cleaner way of
    generating user data for testing purposes. Previous functions for
    generating data have been kept for backwards-compatibility.

--- api/tests/fake_data.py
+++ api/tests/fake_data.py
@@ -1,16 +1,76 @@
+from abc import ABC, abstractmethod, abstractproperty
 from io import BytesIO
 import json
+import random
+import string
 from uuid import uuid4
 from PIL import Image
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from api.models import Animal, PetLastKnownLocation, Sex, Pet
+from api.models import Animal, Sex, Pet
+
+
+class FakeData:
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        self.fields = []
+        for key in self.__dict__:
+            if key != "kwargs" and key != "fields":
+                self.fields.append(key)
+
+    @property
+    def data(self) -> dict:
+        data = dict()
+        for field in self.fields:
+            data[field] = self.kwargs.get(field, getattr(self, field))
+        return data
+
+    def exclude(self, fields=[]) -> dict:
+        return {key: value for (key, value) in self.data.items() if key not in fields}
+
+
+class FakePet(FakeData):
+    def __init__(self, **kwargs):
+        self.name = "Yuna"
+        self.type = "Cat"
+        self.age = 2
+        self.sex = Sex.FEMALE
+        self.eye_color = Pet.Color.YELLOW
+        self.color = Pet.Color.BROWN
+        self.weight = 8
+        self.microchip = "900123456789000"
+        super().__init__(**kwargs)
+
+
+class FakeUser(FakeData):
+    def __init__(self, **kwargs):
+        # Use a random string for the username to ensure uniqueness between tests.
+        id = "".join(random.choices(string.ascii_letters + string.digits, k=6))
+        self.username = f"user_{id}"
+        self.first_name = "Bob"
+        self.last_name = "Dylan"
+        self.email = "bob@gmail.com"
+        self.password = fake_password()
+        super().__init__(**kwargs)
+
+
+class FakePost(FakeData):
+    def __init__(self, **kwargs):
+        self.description = "post description"
+        self.location_lat = 20
+        self.location_long = 25
+        self.status = "lost"
+        super().__init__(**kwargs)
 
 
 def fake_pet_data(
-    user, serializable=False, exclude: list = None, format="json", **kwargs
+    user, serializable=False, exclude: list = [], format="json", **kwargs
 ):
+    """
+    Generate pet data for creating model instances and serializers.
 
+
+    """
     location = {"latitude": 31.811348, "longitude": -106.564600}
 
     fields = [
@@ -33,72 +93,63 @@ def fake_pet_data(
         {"name": "user", "default": user},
     ]
 
-    data = dict()
-    fields_to_remove = []
+    serializable_defaults = {
+        "last_known_location": {
+            "json": location,
+            # Multipart form data doesn't support nested structures,
+            # so this field's value is converted to a json string.
+            "multipart": json.dumps(location),
+        },
+        "user": {
+            "json": user.id,
+            "multipart": user.id,
+        },
+    }
 
-    if exclude:
-        for field in fields:
-            if field["name"] in exclude:
-                fields_to_remove.append(field)
+    data = dict()
+    fields = list(filter(lambda field: field["name"] not in exclude, fields))
 
     if serializable:
-        # Change any field's non-serializable default value
+        # Change the value of non-serializable fields
         # to one that is serializable.
         for field in fields:
-            if field["name"] == "last_known_location":
-                if format == "json":
-                    field["default"] = {
-                        "latitude": location["latitude"],
-                        "longitude": location["longitude"],
-                    }
-                elif format == "multipart":
-                    # Multipart form data doesn't support nested data,
-                    # so this field's value is converted to a json string.
-                    field["default"] = json.dumps(
-                        {
-                            "latitude": location["latitude"],
-                            "longitude": location["longitude"],
-                        }
-                    )
-            if field["name"] == "user":
-                field["default"] = str(user.id)
+            field_name = field["name"]
+            if field_name in serializable_defaults:
+                field["default"] = serializable_defaults[field_name][format]
 
-    for field in fields_to_remove:
-        fields.remove(field)
-
+    # Populate final data with remaining fields and their values
+    # specified in kwargs or their default values.
     for field in fields:
-        # Populate final data with remaining fields and their values
-        # specified in kwargs or their default values.
         data[field["name"]] = kwargs.get(field["name"], field["default"])
 
     return data
 
 
-def fake_user_data(exclude: list = None, **kwargs):
-    id = str(uuid4())
+def fake_user_data(exclude: list = [], **kwargs) -> dict:
+    """Generate user data for testing."""
+
+    # Use a random string for the username to ensure uniqueness between tests.
+    id = "".join(random.choices(string.ascii_letters + string.digits, k=6))
+
+    final_data = dict()
 
     fields = [
         {"name": "username", "default": f"user_{id}"},
         {"name": "first_name", "default": "oscar"},
         {"name": "last_name", "default": "rojas"},
         {"name": "email", "default": "oscar@email.com"},
-        {"name": "password", "default": "apples"},
+        {"name": "password", "default": fake_password()},
     ]
 
-    data = dict()
-    fields_to_remove = []
-    if exclude:
-        for field in fields:
-            if field["name"] in exclude:
-                fields_to_remove.append(field)
+    # Filter out fields that should be excluded from result.
+    fields = list(filter(lambda field: field["name"] not in exclude, fields))
 
-    for field in fields_to_remove:
-        fields.remove(field)
-
+    # Populate final data with remaining fields and their values
+    # specified in kwargs or their default values.
     for field in fields:
-        data[field["name"]] = kwargs.get(field["name"], field["default"])
+        final_data[field["name"]] = kwargs.get(field["name"], field["default"])
 
-    return data
+    return final_data
 
 
 def fake_image_file():
@@ -106,3 +157,8 @@ def fake_image_file():
     Image.new("RGB", (100, 100)).save(image, "JPEG")
     image.seek(0)
     return SimpleUploadedFile(f"img_{str(uuid4())}.jpg", image.getvalue(), "image/jpeg")
+
+
+def fake_password(length=8):
+    """Generate a random string that can be used as a valid user password for testing."""
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
