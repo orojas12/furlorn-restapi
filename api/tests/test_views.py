8ac7ae0690c4ff750a66222b585fb9fdfd55e6d8
Oscar <orojas12@outlook.com>
2022-04-26 16:09:29 -0600

    restructure views to reflect changes in model
    These changes also include views for user login and logout.

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,220 +1,310 @@
+import json
 from unittest.mock import patch
 
-from api.models import Pet, User
-from api.tests.fake_data import fake_image_file, fake_pet_data, fake_user_data
-from api.views import PetList, UserDetail, UserList
+from api.models import Animal, Pet, User, Post, Breed
+from api.tests.fake_data import (
+    FakeUser,
+    FakePet,
+    FakePost,
+    fake_image_file,
+)
+from api.tests.exceptions import TestException
+from api.views import PostView, PostsView, UserView, RegisterUserView
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIRequestFactory, APIClient, force_authenticate
 
 
-class UserListTest(TestCase):
+class RegisterUserViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.url = reverse("user-list")
+        cls.url = reverse("register_user")
         cls.factory = APIRequestFactory()
 
     def test_post_201_response(self):
-        data = fake_user_data()
+        data = FakeUser().data
         request = self.factory.post(self.url, data, format="json")
-        response = UserList.as_view()(request)
+        response = RegisterUserView.as_view()(request)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
     def test_post_400_response(self):
-        data = fake_user_data(exclude=["password"])
+        data = FakeUser().exclude(["password"])
         request = self.factory.post(self.url, data, format="json")
-        response = UserList.as_view()(request)
+        response = RegisterUserView.as_view()(request)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
-    @patch("api.views.UserSerializer.save")
+    @patch("api.views.RegisterUserSerializer.save")
     def test_post_500_response(self, mock_save):
-        mock_save.side_effect = Exception("test exception")
-        data = fake_user_data()
+        mock_save.side_effect = TestException()
+        data = FakeUser().data
         request = self.factory.post(self.url, data, format="json")
-        response = UserList.as_view()(request)
+        response = RegisterUserView.as_view()(request)
         self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
 
-class UserDetailTest(TestCase):
+class UserViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.factory = APIRequestFactory()
-        cls.user = User.objects.create(**fake_user_data())
-        cls.kwargs = {"pk": cls.user.id}
-        cls.url = reverse("user-detail", kwargs=cls.kwargs)
+        cls.user = User.objects.create_user(**FakeUser().data)
+        cls.user2 = User.objects.create_user(**FakeUser().data)
+        cls.kwargs = {"username": cls.user.username}
+        cls.url = reverse("user", kwargs=cls.kwargs)
 
     def test_get_200_response(self):
         request = self.factory.get(self.url)
-        response = UserDetail.as_view()(request, **self.kwargs)
+        force_authenticate(request, self.user)
+        response = UserView.as_view()(request, **self.kwargs)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
     def test_get_404_response(self):
         request = self.factory.get(self.url)
-        response = UserDetail.as_view()(request, pk=0)  # non-existing pk
+        force_authenticate(request, self.user)
+        response = UserView.as_view()(request, username=0)  # non-existing username
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        self.assertIsInstance(response.data, dict)
-        self.assertNotEqual(len(response.data), 0)
-
-    def test_patch_200_response(self):
-        data = {"email": "newemail@email.com"}
-        request = self.factory.patch(self.url, data, format="json")
-        response = UserDetail.as_view()(request, **self.kwargs)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIsInstance(response.data, dict)
-        self.assertNotEqual(len(response.data), 0)
-
-    def test_patch_404_response(self):
-        data = {"user": {"email": "newemail@email.com"}}
-        request = self.factory.patch(self.url, data, format="json")
-        response = UserDetail.as_view()(request, pk=0)  # non-existing pk
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        self.assertIsInstance(response.data, dict)
-        self.assertNotEqual(len(response.data), 0)
-
-    def test_patch_400_response(self):
-        data = {"user_data": {"email": "newemail@email.com"}}  # invalid key name
-        request = self.factory.patch(self.url, data, format="json")
-        response = UserDetail.as_view()(request, **self.kwargs)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIsInstance(response.data, dict)
-        self.assertNotEqual(len(response.data), 0)
-
-    @patch("api.views.UserSerializer.save")
-    def test_patch_500_response(self, mock_save):
-        mock_save.side_effect = Exception("test exception")
-        data = fake_user_data(exclude=["email"])
-        request = self.factory.patch(self.url, data, format="json")
-        response = UserDetail.as_view()(request, **self.kwargs)
-        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
     def test_put_200_response(self):
-        data = fake_user_data(username="updateduser1")
+        data = FakeUser().exclude(["password"])
         request = self.factory.put(self.url, data, format="json")
-        response = UserDetail.as_view()(request, **self.kwargs)
+        force_authenticate(request, self.user)
+        response = UserView.as_view()(request, **self.kwargs)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
-    def test_put_404_response(self):
-        data = fake_user_data(username="updateduser1")
+    def test_put_403_response(self):
+        data = FakeUser().exclude(["password"])
         request = self.factory.put(self.url, data, format="json")
-        response = UserDetail.as_view()(request, pk=0)
+        force_authenticate(request, self.user2)
+        response = UserView.as_view()(request, **self.kwargs)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertIsInstance(response.data, dict)
+        self.assertNotEqual(len(response.data), 0)
+
+    def test_put_404_response(self):
+        data = FakeUser().exclude(["password"])
+        request = self.factory.put(self.url, data, format="json")
+        force_authenticate(request, self.user)
+        response = UserView.as_view()(request, username=0)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
     def test_put_400_response(self):
-        data = fake_user_data(exclude=["email"])
+        data = FakeUser().exclude(["email"])
         request = self.factory.put(self.url, data, format="json")
-        response = UserDetail.as_view()(request, **self.kwargs)
+        force_authenticate(request, self.user)
+        response = UserView.as_view()(request, **self.kwargs)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
     @patch("api.views.UserSerializer.save")
     def test_put_500_response(self, mock_save):
-        mock_save.side_effect = Exception("test exception")
-        data = fake_user_data(username="updateduser1")
+        mock_save.side_effect = TestException()
+        data = FakeUser().exclude(["password"])
         request = self.factory.put(self.url, data, format="json")
-        response = UserDetail.as_view()(request, **self.kwargs)
+        force_authenticate(request, self.user)
+        response = UserView.as_view()(request, **self.kwargs)
         self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
     def test_delete_204_response(self):
         request = self.factory.delete(self.url)
-        response = UserDetail.as_view()(request, **self.kwargs)
+        force_authenticate(request, self.user)
+        response = UserView.as_view()(request, **self.kwargs)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(response.data, None)
 
     def test_delete_404_response(self):
         request = self.factory.delete(self.url)
-        response = UserDetail.as_view()(request, pk=0)
+        force_authenticate(request, self.user)
+        response = UserView.as_view()(request, username=0)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
     @patch("api.views.User.delete")
     def test_delete_500_response(self, mock_delete):
-        mock_delete.side_effect = Exception("test exception")
+        mock_delete.side_effect = TestException()
         request = self.factory.delete(self.url)
-        response = UserDetail.as_view()(request, **self.kwargs)
+        force_authenticate(request, self.user)
+        response = UserView.as_view()(request, **self.kwargs)
         self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
 
-class PetListTest(TestCase):
+class PostsViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = User.objects.create(**fake_user_data())
+        cls.user = User.objects.create_user(**FakeUser().data)
+        cls.breed = Breed.objects.create(name="American Shorthair", animal=Animal.CAT)
+        cls.pet = Pet.objects.create(**FakePet().data)
+        cls.pet.breed.set([cls.breed])
         for _ in range(3):
-            Pet.objects.create(**fake_pet_data(cls.user))
+            Post.objects.create(pet=cls.pet, user=cls.user, **FakePost().data)
 
-        cls.url = reverse("pet-list")
+        cls.url = reverse("posts")
         cls.factory = APIRequestFactory()
 
     def test_get_200_response(self):
         request = self.factory.get(self.url)
-        response = PetList.as_view()(request)
+        response = PostsView.as_view()(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, list)
-        self.assertEqual(len(response.data), len(Pet.objects.all()))
+        self.assertEqual(len(response.data), len(Post.objects.all()))
 
     def test_post_201_response(self):
-        # TODO: change other tests to use multipart data
-        data = fake_pet_data(self.user, serializable=True, format="multipart")
+        data = FakePost().data
+        data["pet"] = json.dumps(FakePet().data)
         data["photos"] = [
             fake_image_file(),
             fake_image_file(),
             fake_image_file(),
         ]
         request = self.factory.post(self.url, data)
-        response = PetList.as_view()(request)
+        force_authenticate(request, self.user)
+        response = PostsView.as_view()(request)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
     def test_post_400_response(self):
-        invalid_data = fake_pet_data(
-            self.user,
-            serializable=True,
-            exclude=["last_known_location"],
-            format="multipart",
-        )
+        invalid_data = FakePost().data
         invalid_data["photos"] = [
             fake_image_file(),
             fake_image_file(),
             fake_image_file(),
         ]
         request = self.factory.post(self.url, data=invalid_data)
-        response = PetList.as_view()(request)
+        force_authenticate(request, self.user)
+        response = PostsView.as_view()(request)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
 
-    @patch("api.views.PetSerializer.save")
+    @patch("api.views.CreatePostSerializer.save")
     def test_post_500_response(self, mock_save):
-        mock_save.side_effect = Exception("test exception")
-        data = fake_pet_data(self.user, serializable=True, format="multipart")
+        mock_save.side_effect = TestException()
+        data = FakePost().data
+        data["pet"] = json.dumps(FakePet().data)
         data["photos"] = [
             fake_image_file(),
             fake_image_file(),
             fake_image_file(),
         ]
         request = self.factory.post(self.url, data)
-        response = PetList.as_view()(request)
+        force_authenticate(request, self.user)
+        response = PostsView.as_view()(request)
         self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
         self.assertIsInstance(response.data, dict)
         self.assertNotEqual(len(response.data), 0)
+
+
+class PostViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(**FakeUser().data)
+        cls.pet = Pet.objects.create(**FakePet().data)
+        cls.post = Post.objects.create(user=cls.user, pet=cls.pet, **FakePost().data)
+        cls.kwargs = {"pk": cls.post.id}
+        cls.url = reverse("post", kwargs=cls.kwargs)
+        cls.factory = APIRequestFactory()
+
+    def test_get_200_response(self):
+        request = self.factory.get(self.url)
+        response = PostView.as_view()(request, **self.kwargs)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, dict)
+        self.assertNotEqual(len(response.data), 0)
+
+    def test_get_404_response(self):
+        request = self.factory.get(self.url)
+        response = PostView.as_view()(request, pk=-1)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIsInstance(response.data, dict)
+        self.assertNotEqual(len(response.data), 0)
+
+    def test_put_200_response(self):
+        new_description = "new description"
+        data = FakePost(description=new_description).data
+        request = self.factory.put(self.url, data, format="json")
+        force_authenticate(request, self.user)
+        response = PostView.as_view()(request, **self.kwargs)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["description"], new_description)
+
+    def test_put_404_response(self):
+        data = FakePost().data
+        request = self.factory.put(self.url, data, format="json")
+        force_authenticate(request, self.user)
+        response = PostView.as_view()(request, pk=-1)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIsInstance(response.data, dict)
+        self.assertNotEqual(len(response.data), 0)
+
+    def test_delete_204_response(self):
+        request = self.factory.delete(self.url)
+        force_authenticate(request, self.user)
+        response = PostView.as_view()(request, **self.kwargs)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_404_response(self):
+        request = self.factory.delete(self.url)
+        force_authenticate(request, self.user)
+        response = PostView.as_view()(request, pk=-1)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIsInstance(response.data, dict)
+        self.assertNotEqual(len(response.data), 0)
+
+
+class LoginUserViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user_data = FakeUser().data
+        cls.user_password = user_data["password"]
+        cls.user = User.objects.create_user(**user_data)
+        cls.client = APIClient(enforce_csrf_checks=True)
+        cls.url = reverse("login_user")
+
+    def test_login(self):
+        "Test that login is successful and csrf and session cookies are set correctly."
+        response = self.client.post(
+            self.url,
+            data={
+                "username": self.user.username,
+                "password": self.user_password,
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.cookies), 2)
+        self.assertIn("csrftoken", response.cookies)
+        self.assertIn("sessionid", response.cookies)
+
+
+class LogoutUserViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user_data = FakeUser().data
+        cls.user_password = user_data["password"]
+        cls.user = User.objects.create_user(**user_data)
+        cls.client = APIClient(enforce_csrf_checks=True)
+        cls.url = reverse("logout_user")
+
+    def test_logout(self):
+        self.client.login(username=self.user.username, password=self.user_password)
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- api/views.py
+++ api/views.py
@@ -1,117 +1,202 @@
+import logging
 from django.core.exceptions import ObjectDoesNotExist
-from django.db import models
-from rest_framework import status, views
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
 
-from api.models import Pet, User
-from api.serializers import PetSerializer, UserSerializer
+from api.models import User, Post
+from api.serializers import (
+    CreatePostSerializer,
+    RegisterUserSerializer,
+    UserSerializer,
+    PostSerializer,
+    LoginUserSerializer,
+)
 from api.parsers import MultiPartJSONParser
+from api.permissions import IsOwnerOrReadOnly, IsSameUser
+
+logger = logging.getLogger(__name__)
 
 
-class UserList(views.APIView):
-    """
-    A view class for creating new users.
-    """
-
-    parser_classes = [JSONParser]
-
-    def post(self, request, format="json"):
-        serializer = UserSerializer(data=request.data)
-        if not serializer.is_valid():
-            return response_400(serializer.errors)
-        try:
-            serializer.save()
-        except Exception as e:
-            print(e)
-            return response_500()
-        return response_201(serializer.data)
-
-
-class UserDetail(views.APIView):
+class UserView(APIView):
     """
     A view class for reading/updating user data, and for deleting users.
     """
 
+    permission_classes = [IsAuthenticated, IsSameUser]
     parser_classes = [JSONParser]
 
-    def get(self, request, pk=None):
-        user = get_obj(User, pk)
-        if user is None:
+    def get(self, request, username=None):
+        try:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist:
             return response_404()
+
         serializer = UserSerializer(user)
         return response_200(serializer.data)
 
-    def patch(self, request, pk=None):
-        user = get_obj(User, pk)
-        if user is None:
-            return response_404()
-
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        if not serializer.is_valid():
-            return response_400(serializer.errors)
+    def put(self, request, username=None):
         try:
-            serializer.save()
-        except Exception as e:
-            print(e)
-            return response_500()
-        return response_200(serializer.data)
-
-    def put(self, request, pk=None):
-        user = get_obj(User, pk)
-        if user is None:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist:
             return response_404()
 
+        self.check_object_permissions(request, user)
         serializer = UserSerializer(user, data=request.data)
-        if not serializer.is_valid():
+        if serializer.is_valid():
+            try:
+                serializer.save()
+                return response_200(serializer.data)
+            except Exception as exc:
+                logger.exception(exc)
+                return response_500()
+        else:
             return response_400(serializer.errors)
-        try:
-            serializer.save()
-        except Exception as e:
-            print(e)
-            return response_500()
-        return response_200(serializer.data)
 
-    def delete(self, request, pk=None):
-        user = get_obj(User, pk)
-        if user is None:
+    def delete(self, request, username=None):
+        try:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist:
             return response_404()
+
+        self.check_object_permissions(request, user)
 
         try:
             user.delete()
             return response_204()
-        except Exception as e:
-            print(e)
+        except Exception as exc:
+            logger.exception(exc)
             return response_500()
 
 
-class PetList(views.APIView):
-    """A View class for searching for lost and found pets and creating new posts."""
+class PostsView(APIView):
+    """A View class for reading and creating new posts."""
 
+    permission_classes = [IsAuthenticatedOrReadOnly]
     parser_classes = [MultiPartJSONParser]
 
     def get(self, request):
-        pets = Pet.objects.all()
-        serializer = PetSerializer(pets, many=True)
+        posts = Post.objects.select_related("pet", "user").prefetch_related("photos")
+        serializer = PostSerializer(posts, many=True)
         return response_200(serializer.data)
 
     def post(self, request):
         data = request.data
-        for i, file in enumerate(data["photos"]):
-            data["photos"][i] = {"order": i, "file": file}
-        serializer = PetSerializer(data=request.data)
+        data["photos"] = [
+            {"order": i, "file": file} for i, file in enumerate(data["photos"])
+        ]
+        serializer = CreatePostSerializer(data=data, context={"user": request.user})
+
+        try:
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return response_201(serializer.data)
+        except ValidationError as exc:
+            return response_400(serializer.errors)
+        except Exception as exc:
+            logger.exception(exc)
+            return response_500()
+
+
+class PostView(APIView):
+    """A View class for retrieve/update/delete for a pet."""
+
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    parser_classes = [JSONParser]
+
+    def get(self, request, pk=None):
+        try:
+            post = (
+                Post.objects.select_related("pet").prefetch_related("photos").get(pk=pk)
+            )
+        except ObjectDoesNotExist:
+            return response_404()
+
+        serializer = PostSerializer(post)
+        return response_200(serializer.data)
+
+    def put(self, request, pk=None):
+        try:
+            post = Post.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            return response_404()
+
+        self.check_object_permissions(request, post)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            try:
+                serializer.save()
+                return response_200(serializer.data)
+            except Exception as exc:
+                logger.exception(exc)
+                return response_500()
+        else:
+            return response_400(serializer.errors)
+
+    def delete(self, request, pk=None):
+        try:
+            post = Post.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            return response_404()
+
+        self.check_object_permissions(request, post)
+        try:
+            post.delete()
+            return response_204()
+        except Exception as exc:
+            logger.exception(exc)
+            return response_500()
+
+
+class RegisterUserView(APIView):
+    """A View class for registering new users."""
+
+    parser_classes = [JSONParser]
+
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                serializer.save()
+                return response_201(serializer.data)
+            except Exception as exc:
+                logger.exception(exc)
+                return response_500()
+        else:
+            return response_400(serializer.errors)
+
+
+class LoginUserView(APIView):
+    """A View class for handling login with sessions."""
+
+    parser_classess = [JSONParser]
+
+    def post(self, request):
+        serializer = LoginUserSerializer(data=request.data)
         if not serializer.is_valid():
             return response_400(serializer.errors)
-        try:
-            serializer.save()
-        except Exception as e:
-            print(e)
-            return response_500()
-        return response_201(serializer.data)
+        username = request.data["username"]
+        password = request.data["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return response_200(serializer.data)
+        else:
+            return response_403()
 
 
-class PetDetail(views.APIView):
-    pass
+class LogoutUserView(APIView):
+    """A View class for handling logout with sessions."""
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return response_200({})
 
 
 def response_200(data):
@@ -130,6 +215,13 @@ def response_400(data):
     return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
 
 
+def response_403():
+    return Response(
+        data={"detail": "Username or password is incorrect."},
+        status=status.HTTP_403_FORBIDDEN,
+    )
+
+
 def response_404():
     return Response(
         data={"detail": "Object not found."}, status=status.HTTP_404_NOT_FOUND
@@ -141,17 +233,3 @@ def response_500():
         data={"detail": "Server error. Please try again later."},
         status=status.HTTP_500_INTERNAL_SERVER_ERROR,
     )
-
-
-def get_obj(model: models.Model, pk):
-    try:
-        return model.objects.get(pk=pk)
-    except ObjectDoesNotExist:
-        return None
-
-
-def delete_obj(model: models.Model, pk):
-    try:
-        return model.objects.get(pk=pk).delete()
-    except ObjectDoesNotExist:
-        return None
