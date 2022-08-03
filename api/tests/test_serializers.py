2af33f5df94d13b412a26822add5fce2b52c8f63
Oscar <orojas12@outlook.com>
2022-08-02 21:12:19 -0600

    add test case for CreatePostSerializer

--- api/tests/test_serializers.py
+++ api/tests/test_serializers.py
@@ -1,6 +1,8 @@
-from api.models import Pet, Photo, User, Post
+from unittest.mock import MagicMock, patch
+from api.models import Breed, Pet, Photo, Species, User, Post
 from api.serializers import (
     ChangePasswordSerializer,
+    CreatePostSerializer,
     PetSerializer,
     PhotoSerializer,
     UserSerializer,
@@ -160,3 +162,51 @@ class ChangePasswordSerializerTest(TestCase):
         self.assertTrue(serializer.is_valid())
         serializer.save()
         self.assertTrue(self.user.check_password(self.new_password))
+
+
+class CreatePostSerializerTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.fields = [
+            "description",
+            "location_lat",
+            "location_long",
+            "status",
+            "pet",
+            "photos",
+        ]
+        cls.user = User.objects.create(**FakeUser().data)
+        cls.cat_breeds = Breed.objects.filter(species=Species.CAT)
+        cls.dog_breeds = Breed.objects.filter(species=Species.DOG)
+        cls.pet_data = FakePet().data
+        cls.post_data = FakePost().data
+        cls.data = {
+            "pet": {
+                "breed": [cls.cat_breeds[0].id, cls.cat_breeds[1].id],
+                **cls.pet_data,
+            },
+            "photos": [
+                {"order": 0, "file": fake_image_file()},
+                {"order": 1, "file": fake_image_file()},
+            ],
+            **cls.post_data,
+        }
+
+    def test_deserializes_fields(self):
+        serializer = CreatePostSerializer(data=self.data)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        self.assertEqual(len(serializer.validated_data), len(self.fields))
+
+    @patch("api.models.Photo.objects.create")
+    def test_creates_post(
+        self,
+        mock_photo_create: MagicMock,
+    ):
+        serializer = CreatePostSerializer(data=self.data, context={"user": self.user})
+        serializer.is_valid(raise_exception=True)
+        post = serializer.save()
+        pet = Pet.objects.all().first()
+        self.assertEqual(post, Post.objects.all().first())
+        self.assertEqual(post.pet, pet)
+        self.assertEqual(pet.breed.count(), 2)
+        self.assertEqual(mock_photo_create.call_count, 2)
